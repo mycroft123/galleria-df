ea4bd306ce073259707930672aea0c5af6bb215f
mycroft123 <35017678+mycroft123@users.noreply.github.com>
2024-12-26 14:37:54 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
     images: {
         domains: ['bafybeiey74fhfsbksvnzlgdix4vilwcbuvs32oc2gdd4pksvrezhn7kw7e.ipfs.nftstorage.link'],
     },
+    experimental: {
+        runtime: 'edge',
+        serverActions: true,
+    }
 };
 
 module.exports = nextConfig;
