424245faccf1e117870b21801c63cffa71962ce6
mycroft123 <mycroft55@hotmail.com>
2025-05-19 17:43:46 -0400

    Add MongoDB connection utility

---
+++ app/lib/mongodb.ts
@@ -0,0 +1,35 @@
+// lib/mongodb.ts
+import { MongoClient } from 'mongodb';
+
+const uri = process.env.MONGODB_URI || '';
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+};
+
+let client;
+let clientPromise;
+
+if (!uri) {
+  throw new Error('Please add your Mongo URI to .env.local');
+}
+
+// In development mode, use a global variable to preserve connection across reloads
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  // In production mode, create a new client for each connection
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export async function connectToDatabase() {
+  const client = await clientPromise;
+  const db = client.db(); // If you have a specific DB name, use client.db('your-db-name')
+  
+  return { client, db };
+}
